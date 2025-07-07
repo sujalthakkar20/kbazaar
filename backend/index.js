c63653eb67e50a5edc3157d4ca589cf74152ddab
Sujal Thakkar <sujalthakkar001@gmail.com>
2025-07-07 16:48:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Frontend URL
+    origin: 'https://kbazaar-lake.vercel.app/', // Frontend URL
     credentials: true,
 };
 app.use(cors(corsOptions));
